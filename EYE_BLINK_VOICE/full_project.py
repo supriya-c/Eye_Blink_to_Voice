09e90979c02d9ebd1b1f08da08f579a80565aa4c
supriya-c <43084547+supriya-c@users.noreply.github.com>
2020-06-10 13:01:18 +0530

    Update full_project.py

--- EYE_BLINK_VOICE/full_project.py
+++ EYE_BLINK_VOICE/full_project.py
@@ -8,8 +8,8 @@ from itertools import cycle
 from PIL import Image, ImageTk
 from twilio.rest import Client
 
-account_sid = "TWILIO_ACCOUNT_SID"
-auth_token = "TWILIO_AUTH_TOKEN"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 
@@ -107,123 +107,123 @@ class Slideshow(Tkinter.Tk):
                 if self.image_name=="yes.jpg":
                     if flag==1:
                         print("I'm hungry, need food")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="Iam Hungry!!!Feed me")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="Iam Hungry!!!Feed me")
                     call = client.calls.create(
                         twiml='<Response><Say>Iam Hungry!Feed me food.</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==2:
                         print("I'm thristy, need some water")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="Iam Thirsty")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="Iam Thirsty")
                     call = client.calls.create(
                         twiml='<Response><Say>Iam thristy,need water</Say></Response>',
-                        to='CONTACT_NO', 
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'], 
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==3:
                         print("I need snacks")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want Snacks")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want Snacks")
                     call = client.calls.create(
                         twiml='<Response><Say>Snack time, give me chrunchy things!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==4:
                         print("I wanna watch TV")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want to watch TV")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want to watch TV")
                     call = client.calls.create(
                         twiml='<Response><Say>I need to watch Television</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==5:
                         print("I wanna make a call")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want to make a call")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want to make a call")
                     call = client.calls.create(
                         twiml='<Response><Say>I wanna make a call!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==6:
                         print("I need to pee")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want to use washroom")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want to use washroom")
                     call = client.calls.create(
                         twiml='<Response><Say>I need to pee!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==7:
                         print("I need coffee")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want to drink coffee or tea")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want to drink coffee or tea")
                     call = client.calls.create(
                         twiml='<Response><Say>I want to drink coffee or tea</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                    print(call.sid)
                     if flag==8:
                         print("I wanna drink milk")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want to drink milk")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want to drink milk")
                     call = client.calls.create(
                         twiml='<Response><Say>I want to drink milk</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==9:
                         print("I want some fruits")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want fruits")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want fruits")
                     call = client.calls.create(
                         twiml='<Response><Say>I want fruits!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==10:
                         print("I want some juice")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want juice")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO"],body="I want juice")
                     call = client.calls.create(
                         twiml='<Response><Say>I want juice!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==11:
                         print("It's an emergency")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="Emergency!Call the doctor")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="Emergency!Call the doctor")
                     call = client.calls.create(
                         twiml='<Response><Say>Its an Emergency!Please call the doctor!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==12:
                         print("I want curd rice")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want curdrice to eat")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want curdrice to eat")
                     call = client.calls.create(
                         twiml='<Response><Say>Its an Emergency!Please call the doctor!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==13:
                         print("I want idli sambar")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want idli sambar to eat")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want idli sambar to eat")
                     call = client.calls.create(
                         twiml='<Response><Say>I want idli sambar to eat!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==14:
                         print("I want ragi malt")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want ragi malt")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want ragi malt")
                     call = client.calls.create(
                         twiml='<Response><Say>I want ragi malt!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
                     if flag==15:
                         print("I want khichdi to eat")
-                        client.api.account.messages.create(to="CONTACT_NO",from_="TWILIO_NO",body="I want khichdi to eat")
+                        client.api.account.messages.create(to=os.environ['CONTACT_NO'],from_=os.environ['TWILIO_NO'],body="I want khichdi to eat")
                     call = client.calls.create(
                         twiml='<Response><Say>I want khichdi to eat!</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO')
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO'])
                     print(call.sid)
       
         else:
