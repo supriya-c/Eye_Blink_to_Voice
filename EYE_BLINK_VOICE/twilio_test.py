20136062b5d3a671d211ed7c9f8fd34ff7db226f
supriya-c <43084547+supriya-c@users.noreply.github.com>
2020-06-10 12:48:13 +0530

    Update twilio_test.py

--- EYE_BLINK_VOICE/twilio_test.py
+++ EYE_BLINK_VOICE/twilio_test.py
@@ -1,21 +1,21 @@
 #Text and Voice Message
 
 from twilio.rest import Client
-account_sid = "TWILIO_ACOOUNT_SID"
-auth_token = "TWILIO_AUTH_TOKEN"
+account_sid = os.environ['TWILIO_ACOOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 #Text Message
 client.api.account.messages.create(
-    to="CONTACT_NO",
-    from_="TWILIO_NO" ,
+    to=os.environ['CONTACT_NO'],
+    from_=os.environ['TWILIO_NO'],
     body=" Text Message" )
 
 #Voice Message
 call = client.calls.create(
                         twiml='<Response><Say>Selected message for voice call</Say></Response>',
-                        to='CONTACT_NO',
-                        from_='TWILIO_NO'
+                        to=os.environ['CONTACT_NO'],
+                        from_=os.environ['TWILIO_NO']
                     )
 
 print(call.sid)
